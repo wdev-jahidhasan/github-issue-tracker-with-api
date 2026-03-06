98b6088c3a63c3e62bdf1ca8e1fb30701982a218
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-03-07 03:07:11 +0600

    navbar is responsive now

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-alert('JS file added');
+
+
+// login related codes 
+
+// const username = document.getElementById('username');
+// const password = document.getElementById('password');
+// const loginBtn = document.getElementById('login');
+// loginBtn.addEventListener('click', function() {
+//   if(username.value === 'admin' && password.value === 'admin123'){
+//     document.getElementById('main-page').classList.remove('hidden');
+//     document.getElementById('login-page').classList.add('hidden');
+//   }else{
+//     alert('Wrong Credentials. Please, enter right username and password');
+//   }
+// });
